3b3848a3f23d27fddcd08334503a5701a116fb41
uehara-beep <uehara@sunyutech.jp>
2026-01-23 21:49:29 +0900

    Fix puma rackup config for Puma 7

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,14 @@
-rackup DefaultRackup
+# config/puma.rb
+
 environment ENV.fetch("RAILS_ENV") { "production" }
 
+# RailwayはPORTを渡す
 port ENV.fetch("PORT") { 3000 }
 bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 3000 }}"
 
+# ここが重要：DefaultRackup は使わない
+rackup "config.ru"
+
 threads 3, 3
 workers 1
-
 preload_app!
