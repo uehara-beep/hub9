c8ae0a026a9a29837dcfb2bbde7069862306e2a0
uehara-beep <uehara@sunyutech.jp>
2026-01-23 21:09:09 +0900

    Fix Puma bind and PORT for Railway

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-port ENV.fetch("PORT", 8080)
-bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 8080)}"
-environment ENV.fetch("RAILS_ENV", "production")
-plugin :tmp_restart
+# config/puma.rb
+port ENV.fetch("PORT") { 3000 }
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 3000 }}"
+environment ENV.fetch("RAILS_ENV") { "production" }
