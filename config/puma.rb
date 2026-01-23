81f35ee9a7fe36592858df59e7d6e5e6f2ed8741
uehara-beep <uehara@sunyutech.jp>
2026-01-23 21:21:44 +0900

    Update Puma config with workers and threads

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,10 @@
-# config/puma.rb
+rackup DefaultRackup
+environment ENV.fetch("RAILS_ENV") { "production" }
+
 port ENV.fetch("PORT") { 3000 }
 bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 3000 }}"
-environment ENV.fetch("RAILS_ENV") { "production" }
+
+threads 3, 3
+workers 1
+
+preload_app!
