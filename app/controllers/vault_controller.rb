c61e4159d7c6aadaab8b8ab0ac7ae43311b17b37
uehara-beep <uehara@sunyutech.jp>
2026-01-24 09:38:26 +0900

    Add no-store cache headers to Vault

--- app/controllers/vault_controller.rb
+++ app/controllers/vault_controller.rb
@@ -1,7 +1,16 @@
 class VaultController < ApplicationController
+  before_action :no_store
+
   def index
   end
 
   def home
+  end
+
+  private
+
+  def no_store
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
   end
 end
